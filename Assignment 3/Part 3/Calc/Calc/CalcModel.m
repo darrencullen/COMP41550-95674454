5fa25d07584809e0ee6bb68615d07562031f60a2
DC <darrencullen@gmail.com>
2013-02-12 23:49:51 +0000

    3.3.1 initial setup

---
+++ Assignment 3/Part 3/Calc/Calc/CalcModel.m
@@ -0,0 +1,179 @@
+//
+//  CalcModel.m
+//  Calc
+//
+//  Created by darren cullen on 28/01/2013.
+//  Copyright (c) 2013 95674454. All rights reserved.
+//
+
+#import "CalcModel.h"
+#import "DDMathParser.h"
+
+@interface CalcModel()
+@property (nonatomic) BOOL doesExpressionHaveVariable;
+@property (nonatomic) int latestVariableIndex;
+@end
+
+@implementation CalcModel
+
+- (id)init
+{
+    if (self = [super init])
+    {
+        _expression = [[NSMutableArray alloc] init];
+    }
+    return self;
+}
+
++ (double)evaluateExpression:(id)anExpression usingVariableValues:(NSDictionary *)variable
+{
+    NSMutableString *expressionToEvaluate = [[NSMutableString alloc] init];
+    
+    // TODO at this point, use waiting operand etc
+    // loop. if numeric - set operand. if not - performoperation.
+    
+    for (NSString *item in anExpression) {
+        if (variable[item]){
+            [expressionToEvaluate appendString:[variable objectForKey:item]];
+        } else {
+            [expressionToEvaluate appendString:item];
+        }
+        NSLog(@"expression: %@:",expressionToEvaluate);
+    }
+    
+    expressionToEvaluate = [[expressionToEvaluate stringByReplacingOccurrencesOfString:@"="
+                                                                            withString:@""]
+                             mutableCopy];
+    
+    double result = [[expressionToEvaluate numberByEvaluatingString] doubleValue];
+    return result;
+}
+
++ (NSSet *)variablesInExpression:(id)anExpression
+{
+    NSMutableSet *expressionVariables = [[NSMutableSet alloc] init];
+    
+    for (NSString *item in anExpression) {
+        if (([item isEqual:@"a"]) || ([item isEqual:@"b"]) || ([item isEqual:@"x"])){
+            if (![expressionVariables containsObject:item])
+                [expressionVariables addObject:item];
+        }
+    }
+    
+    if ([expressionVariables count] == 0)
+        return nil;
+    else
+        return expressionVariables;
+}
+
++ (id)propertyListForExpression:(id)anExpression
+{
+    // TODO:
+    return nil;
+}
+
+- (double)performOperation:(NSString *)operation
+{
+    _operationError = NO;
+    
+    if (([self.waitingOperation isEqual:@"/"]) && (self.operand == 0)){
+        _operationError = YES;
+        _operationErrorMessage = @"Division by zero not allowed";
+        self.operand = 0;
+        self.waitingOperand = 0;
+        self.waitingOperation = nil;
+        return 0;
+    }
+    
+    if (([operation isEqual:@"sqrt"]) && (self.operand < 0)){
+        _operationError = YES;
+        _operationErrorMessage = @"Square root of negative number not allowed";
+        self.operand = 0;
+        self.waitingOperand = 0;
+        self.waitingOperation = nil;
+        return 0;
+    }
+    
+    if ([self.waitingOperation isEqual:@"="]){
+        if (![operation isEqual:@"="])
+            [_expression addObject:operation];
+    }
+    else{
+        // only add operand if last object in expression wasn't a variable
+        if ((self.latestVariableIndex != [_expression count]) || (self.latestVariableIndex == 0)){
+            NSString *trimmedOperand = [NSString stringWithFormat:@"%g",self.operand];
+            [_expression addObject:trimmedOperand];
+        }
+        [_expression addObject:operation];
+    }
+
+    
+    // TODO: check for non-negative numbers
+
+    if ([operation isEqual:@"sqrt"])
+        self.operand = sqrt(self.operand);
+    else if ([operation isEqual:@"+/-"])
+        self.operand = - self.operand;
+    else if ([operation isEqual:@"1/x"])
+        self.operand = 1 / self.operand;
+    else if ([operation isEqual:@"sin"])
+        self.operand = sin(self.operand);
+    else if ([operation isEqual:@"cos"])
+        self.operand = cos(self.operand);
+    else if ([operation isEqual:@"mem+"])
+        _valueInMemory = self.valueInMemory + self.operand;
+    else if ([operation isEqual:@"C"]){
+        self.operand = 0;
+        self.waitingOperand = 0;
+        self.waitingOperation = nil;
+        _valueInMemory = 0;
+        [_expression removeAllObjects];
+        self.doesExpressionHaveVariable = NO;
+    }
+    else {
+        [self performWaitingOperation];
+        self.waitingOperation = operation;
+        self.waitingOperand = self.operand;
+    }
+
+    if (self.doesExpressionHaveVariable == YES)
+        return 0;
+    else
+        return self.operand;
+}
+
+- (void)performWaitingOperation
+{      
+    if([@"+" isEqualToString:self.waitingOperation])
+        self.operand = self.waitingOperand + self.operand;
+    else if([@"-" isEqualToString:self.waitingOperation])
+        self.operand = self.waitingOperand - self.operand;
+    else if([@"*" isEqualToString:self.waitingOperation])
+        self.operand = self.waitingOperand * self.operand;
+    else if([@"/" isEqualToString:self.waitingOperation])
+        if(self.operand) self.operand = self.waitingOperand / self.operand;
+}
+
+- (void)setVariableAsOperand:(NSString *)variableName
+{
+    [_expression addObject:variableName];
+    self.doesExpressionHaveVariable = YES;
+    self.latestVariableIndex = [_expression count];
+}
+
+- (NSString *)descriptionOfExpression:(id)anExpression
+{
+    NSMutableString *expressionDescription = [[NSMutableString alloc] init];
+    
+    for (NSString *item in anExpression) {
+        [expressionDescription appendString:item];
+         }
+    return expressionDescription;
+}
+
+- (id)expressionForPropertyList:(id)propertyList
+{
+    // TODO:
+    return nil;
+}
+@end
---
+++ Assignment 3/Part 3/Calc/Calc/CalcViewController.m
@@ -0,0 +1,208 @@
+//
+//  CalcViewController.m
+//  Calc
+//
+//  Created by darren cullen on 28/01/2013.
+//  Copyright (c) 2013 95674454. All rights reserved.
+//
+
+#import "CalcViewController.h"
+
+@interface CalcViewController()
+@property (nonatomic) BOOL isInTheMiddleOfTypingSomething;
+@property (nonatomic) BOOL hasMemoryJustBeenAccessed;
+@property (nonatomic, retain) NSMutableDictionary *variablesSet;
+@property (nonatomic, strong) NSString *variableBeingSet;
+@property (nonatomic, strong) NSMutableSet *variablesCurrentlyInExpression;
+@property (nonatomic) double expressionResult;
+@end
+
+@implementation CalcViewController
+
+
+- (IBAction)digitPressed:(UIButton *)sender
+{
+    NSString *digit = sender.titleLabel.text;
+    
+    // don't allow more than one dot
+    if ([digit isEqual:@"."])
+        if ([self.calcDisplay.text rangeOfString:@"."].location != NSNotFound)
+            return;
+    
+    // handle pi
+    if ([digit isEqual:@"π"])
+        digit = [NSString stringWithFormat:@"%g", M_PI];
+    
+    if (self.isInTheMiddleOfTypingSomething)
+        if (self.hasMemoryJustBeenAccessed == YES)
+            self.calcDisplay.text = digit;
+    
+        // replace digit if 0 displayed or else append digit
+        else if ([self.calcDisplay.text isEqual:@"0"])
+            if ([digit isEqual:@"."])
+                self.calcDisplay.text = [self.calcDisplay.text stringByAppendingString:digit];
+            else
+                self.calcDisplay.text = digit;
+    
+        else
+            self.calcDisplay.text = [self.calcDisplay.text stringByAppendingString:digit];
+    else {
+        if ([digit isEqual:@"."])
+            self.calcDisplay.text = [self.calcDisplay.text stringByAppendingString:digit];
+        else
+            self.calcDisplay.text = digit;
+        self.isInTheMiddleOfTypingSomething = YES;
+    }
+    
+    self.hasMemoryJustBeenAccessed = NO;
+    //self.expressionDisplay.text = [self.expressionDisplay.text stringByAppendingString:digit];
+}
+
+- (IBAction)operationPressed:(UIButton *)sender
+{
+    if (self.isInTheMiddleOfTypingSomething){
+        self.calcModel.operand = [self.calcDisplay.text doubleValue];
+        self.isInTheMiddleOfTypingSomething = NO;
+    }
+    
+    NSString *operation = [[sender titleLabel] text];
+    [self solveEquation:operation];
+//    double result = [[self calcModel] performOperation:operation];
+//    [[self calcDisplay] setText:[NSString stringWithFormat:@"%g", result]];
+//    
+//    
+//    if (self.calcModel.operationError == YES){
+//        UIAlertView *alertDialog;
+//        alertDialog=[[UIAlertView alloc]
+//                     initWithTitle:@"Operation Error"
+//                     message:self.calcModel.operationErrorMessage
+//                     delegate:nil
+//                     cancelButtonTitle:@"OK"
+//                     otherButtonTitles:nil];
+//        [alertDialog show];
+//    }
+//    
+//    self.hasMemoryJustBeenAccessed = NO;
+//    self.expressionDisplay.text = [[self calcModel] descriptionOfExpression:self.calcModel.expression];
+}
+
+- (void) solveEquation:(NSString *)operation
+{
+    double result = [[self calcModel] performOperation:operation];
+    [[self calcDisplay] setText:[NSString stringWithFormat:@"%g", result]];
+    
+    
+    if (self.calcModel.operationError == YES){
+        UIAlertView *alertDialog;
+        alertDialog=[[UIAlertView alloc]
+                     initWithTitle:@"Operation Error"
+                     message:self.calcModel.operationErrorMessage
+                     delegate:nil
+                     cancelButtonTitle:@"OK"
+                     otherButtonTitles:nil];
+        [alertDialog show];
+    }
+    
+    self.hasMemoryJustBeenAccessed = NO;
+    self.expressionDisplay.text = [[self calcModel] descriptionOfExpression:self.calcModel.expression];
+}
+
+- (IBAction)storeValueInMemory:(UIButton *)sender {
+    self.calcModel.valueInMemory = [self.calcDisplay.text doubleValue];
+    self.hasMemoryJustBeenAccessed = YES;
+}
+
+- (IBAction)retrieveValueFromMemory:(UIButton *)sender {
+    [[self calcDisplay] setText:[NSString stringWithFormat:@"%g", self.calcModel.valueInMemory]];
+    self.calcModel.operand = self.calcModel.valueInMemory;
+    self.hasMemoryJustBeenAccessed = YES;
+}
+
+- (IBAction)backspacePressed:(UIButton *)sender
+{
+    if ([self.calcDisplay.text length] > 1)
+        self.calcDisplay.text = [self.calcDisplay.text substringToIndex:[self.calcDisplay.text length] - 1];
+    else if ([self.calcDisplay.text length] == 1)
+        self.calcDisplay.text = @"0";
+}
+
+- (IBAction)variablePressed:(UIButton *)sender
+{
+    [[self calcModel] setVariableAsOperand:sender.titleLabel.text];
+    self.expressionDisplay.text = [[self calcModel] descriptionOfExpression:self.calcModel.expression];
+}
+
+- (IBAction)solveExpressionPressed:(UIButton *)sender {
+//    // if number has been typed before solve clicked then add it to expression
+//    if (self.isInTheMiddleOfTypingSomething){
+//        self.calcModel.operand = [self.calcDisplay.text doubleValue];
+//        self.isInTheMiddleOfTypingSomething = NO;
+//    }        
+    
+    [self.variablesSet removeAllObjects];
+    self.variablesCurrentlyInExpression = [[NSMutableSet alloc] initWithSet:[CalcModel variablesInExpression:self.calcModel.expression]];
+
+    if ([self.variablesCurrentlyInExpression count] > 0){
+        [self promptForVariables];
+    } else {
+        self.expressionResult = [CalcModel evaluateExpression:self.calcModel.expression usingVariableValues:self.variablesSet];
+        [[self calcDisplay] setText:[NSString stringWithFormat:@"%g", self.expressionResult]];
+        
+        return;
+    }
+}
+
+- (void)promptForVariables {
+    
+    if (![self.variablesCurrentlyInExpression count]){
+        //[[self calcDisplay] setText:[NSString stringWithFormat:@"%g", [CalcModel evaluateExpression:self.calcModel.expression usingVariableValues:self.variablesSet]]];
+        
+        self.expressionResult = [CalcModel evaluateExpression:self.calcModel.expression usingVariableValues:self.variablesSet];
+        [[self calcDisplay] setText:[NSString stringWithFormat:@"%g", self.expressionResult]];
+        
+        return;
+        
+    } else {
+        self.variableBeingSet = [self.variablesCurrentlyInExpression anyObject];
+        [self.variablesCurrentlyInExpression removeObject:self.variableBeingSet];
+        
+        UIAlertView *alertDialog;
+        alertDialog = [[UIAlertView alloc] initWithTitle:@"Enter value for variable"
+                                                 message:self.variableBeingSet
+                                                delegate:self
+                                       cancelButtonTitle:@"OK"
+                                       otherButtonTitles:nil];
+        
+        alertDialog.alertViewStyle=UIAlertViewStylePlainTextInput;
+        UITextField * alertTextField = [alertDialog textFieldAtIndex:0];
+        alertTextField.keyboardType = UIKeyboardTypeNumbersAndPunctuation;
+        [alertDialog show];
+    }
+}
+
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex{
+      if (buttonIndex == 0){
+        if ([[alertView textFieldAtIndex:0] text]){         
+            [self.variablesSet setValue:[[alertView textFieldAtIndex:0] text] forKey:self.variableBeingSet];
+        }
+    }
+    [self promptForVariables];
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	self.variablesSet = [[NSMutableDictionary alloc] init];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)viewDidUnload {
+    [self setExpressionDisplay:nil];
+    [super viewDidUnload];
+}
+@end
